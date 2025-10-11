b67bbfdf6db79ac4bcf3e87e02ce206fccd39f57
PHOENIX <PHOENIX@LAPTOP-384EKMOG>
2025-10-11 16:50:52 +0700

    Config dự án theo máy Tân

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 start "Backend" cmd /k "cd /d %~dp0\src && dotnet run"
-start "Frontend" cmd /k "cd /d %~dp0\..\TheGrind5_EventManagement_FrontEnd && npm start"
+start "Frontend" cmd /k "cd /d %~dp0\..\5GrindThe\TheGrind5_EventManagement_FrontEnd && npm start"
